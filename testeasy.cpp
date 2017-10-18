4cd4516284482a55d1daf25d8eae1f4052ca0210
Nime <sebastianz541@googlemail.com>
2017-10-18 17:06:56 +0200

    created a cube in testeasy

--- testeasy.cpp
+++ testeasy.cpp
@@ -37,21 +37,34 @@ namespace
 
         virtual void execute( const Algorithm::Options& options, const volatile bool& /*abortFlag*/ ) override
         {
-          // ##### Variable #####
-          size_t numCellTypes = (size_t) 2;
+          // ##### numCellTypes #####
+          size_t numCellTypes = (size_t) 1;
+
+          // ##### cellCounts #####
           const std::pair< Cell::Type, size_t >  cellCounts0(Cell::HEXAHEDRON, 1);
-          const std::pair< Cell::Type, size_t >  cellCounts1(Cell::TETRAHEDRON, 1);
-          const std::pair< Cell::Type, size_t > cellCounts[2] = {cellCounts0, cellCounts1};
+          //const std::pair< Cell::Type, size_t >  cellCounts1(Cell::TETRAHEDRON, 1);
+          const std::pair< Cell::Type, size_t > cellCounts[1] = {cellCounts0};//, cellCounts1};
+
+          // ##### indices #####
           std::vector<size_t> coords;
-          coords.push_back((size_t)0);
+          coords.push_back((size_t)4);
+          coords.push_back((size_t)5);
           coords.push_back((size_t)1);
+          coords.push_back((size_t)0);
+
+          coords.push_back((size_t)2);
+          coords.push_back((size_t)3);
+          coords.push_back((size_t)7);
+          coords.push_back((size_t)6);
+          //coords.push_back((size_t)1);
           DefaultValueArray< size_t >* indArray = new DefaultValueArray< size_t >( coords, Precision::FLOAT64);
           std::unique_ptr< ValueArray< size_t > > indices((ValueArray<size_t>*) indArray);
           //std::unique_ptr< DefaultValueArray< size_t > > indices( indArray );
+
           // ##### Domain #####
-            long nx = 10;
-            long ny = 10;
-            long nz = 10;
+            long nx = 2;
+            long ny = 2;
+            long nz = 2;
             double dx = 1.0;
             double dy = 1.0;
             double dz = 1.0;
@@ -65,15 +78,11 @@ namespace
 
             std::shared_ptr< const DiscreteDomain< 3 > > mDomain = DomainFactory::makeDomainUniform( extent, origin, spacing );
 
-            //##### Make Object #####
-            std::shared_ptr< const Grid< 3 > > mGrid = DomainFactory::makeGridStructured( *mDomain );
-
-
             //##### New Object #####
-            static std::shared_ptr< const Grid< 3 > > mGridd = DomainFactory::makeGridUnstructured(*mDomain, numCellTypes, cellCounts, std::move(indices));
+            static std::shared_ptr< const Grid< 3 > > mGrid = DomainFactory::makeGridUnstructured(*mDomain, numCellTypes, cellCounts, std::move(indices));
 
             setResult( "grid", mGrid );
         }
     };
-    AlgorithmRegister< CommitTutorialAlgorithm > dummy( "Tutorial/Commit2", "Generate a simple vector field." );
+    AlgorithmRegister< CommitTutorialAlgorithm > dummy( "Test/Cube", "Generate a simple cube grid." );
 }
